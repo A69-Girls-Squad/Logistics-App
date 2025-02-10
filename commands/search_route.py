1b38c718b8c28e5247c78887fee5ba66fb04ec80
DianaValcheva <diana.valcheva@gmail.com>
2025-02-10 15:29:44 +0200

    initial creation

--- commands/search_route.py
+++ commands/search_route.py
@@ -1,4 +1,23 @@
-# iteration through route locations - start location must be before end location
-# departure time in the future
-# free capacity > package weight
-# show expected arrival time as info
+from commands.base_command import BaseCommand
+from commands.validation_helpers import validate_params_count
+from core.application_data import ApplicationData
+from models.route import Route
+
+
+class SearchRouteCommand(BaseCommand):
+    def __init__(self, params, app_data: ApplicationData):
+        validate_params_count(params, 1)
+        super().__init__(app_data)
+        self._params = params
+
+    def execute(self):
+        suitable_routes = []
+        package_id = self._params[0]
+        package = self._app_data.find_package_by_id(package_id)
+        for route in self._app_data.routes:
+            if route.status == Route.STATUS_CREATED and route.free_capacity > package.weight:
+                locations = route.locations
+                if package.start_location in locations and package.end_location in locations:
+                    if locations.index(package.start_location) < locations.index(package.end_location):
+                        suitable_routes.append(str(route))
+        return f"Suitable Routes:\n"+"\n".join(suitable_routes)
--- commands/show_route.py
+++ commands/show_route.py
@@ -1,5 +1,15 @@
-# status in progress
-# stops
-#delivery weight
-# expected current stop
-#
+from commands.base_command import BaseCommand
+from commands.validation_helpers import validate_params_count
+from core.application_data import ApplicationData
+
+
+class ShowRouteCommand(BaseCommand):
+    def __init__(self, params, app_data: ApplicationData):
+        validate_params_count(params, 1)
+        super().__init__(app_data)
+        self._params = params
+
+    def execute(self):
+        route_id = self._params[0]
+        route = self._app_data.find_route_by_id(route_id)
+        return str(route)
