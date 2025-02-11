70c099974c7bd13cdd7ff9082c55c46acdacdf87
DianaValcheva <diana.valcheva@gmail.com>
2025-02-11 15:14:45 +0200

    two new commands

---
+++ commands/show_unassigned_packages.py
@@ -0,0 +1,18 @@
+from commands.base_command import BaseCommand
+from commands.validation_helpers import validate_params_count
+from core.application_data import ApplicationData
+from models.package import Package
+
+
+class ShowUnassignedPackagesCommand(BaseCommand):
+    def __init__(self, params, app_data: ApplicationData):
+        validate_params_count(params, 0)
+        super().__init__(app_data)
+        self._params = params
+
+    def execute(self):
+        unassigned_packages = []
+        for package in self._app_data.packages:
+            if package.status == Package.STATUS.UNASSIGNED:
+                unassigned_packages.append(package)
+        return f"Routs In Progress:\n"+"\n".join(unassigned_packages)
