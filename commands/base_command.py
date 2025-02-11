1e36a0c91a9e42fda9c44582fce879142ccf6bd8
Irina.Parvanova <irina.p.parvanova@gmail.com>
2025-02-11 12:56:08 +0200

    Base Command, Command Factory, Create Package, Show Package implemented

--- commands/base_command.py
+++ commands/base_command.py
@@ -1,2 +1,34 @@
+from application_data import ApplicationData
+
+
 class BaseCommand:
-    pass
+    def __init__(self, params, app_data: ApplicationData):
+        self._params = params
+        self._app_data =app_data
+
+    @property
+    def params(self):
+        return tuple(self._params)
+
+    @property
+    def app_data(self):
+        return self._app_data
+
+    def execute(self):
+        # override in derived classes
+        return ""
+
+    def _expected_params_count(self) -> int:
+        raise NotImplementedError('Override in derived class')
+
+
+    def validate_params_count(self, params, count): # choose validate from here or from the validation_helper?
+        if len(params) != count:
+            raise ValueError(
+                f'Invalid number of arguments. Expected: {count}; received: {len(params)}.")')
+
+    # def _throw_if_user_logged_in(self):
+    #     if self._app_data.has_logged_in_user:
+    #         logged_user = self._app_data.logged_in_user
+    #         raise ValueError(
+    #             f'User {logged_user.username} is logged in! Please log out first!')
