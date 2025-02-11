857923c53eb006ddb8645dde40d7676b9096bd1b
dilyanab <bozhinova.dilyana@gmail.com>
2025-02-11 10:12:05 +0200

    updating eventlog, truck file, all trucks file and search truck command

--- commands/search_truck.py
+++ commands/search_truck.py
@@ -1,4 +1,15 @@
-# truck must be free
-# capacity > load
-# max range > distance
-#
+from models.package import Package
+from models.route import Route
+from models.truck import Truck
+from models.all_trucks import AllTrucks
+
+class SearchTruckCommand:
+    def __init__(self, all_trucks:AllTrucks):
+        self.all_trucks = all_trucks
+        
+    def execute(self, package: Package, route: Route):
+        for truck in self.all_trucks.get_all_trucks():
+            if truck.is_free() and truck.capacity >= package.weight and truck.max_range >= route.distance:
+                return truck
+            else:
+                return f"No available truck found"
--- models/truck.py
+++ models/truck.py
@@ -1,4 +1,4 @@
-from models.package import Package
+import json
 class Truck:
     id_counter = 1001
     def __init__(self, name, capacity, max_range):
@@ -10,6 +10,26 @@ class Truck:
         self._max_range = max_range
         self._assigned_route = None
         self._status = "Free"
+        self._location = None
+    
+    @classmethod
+    def from_json(cls, data):
+        return cls(
+            truck_id=data["id"],
+            name=data["name"],
+            capacity=data["capacity"],
+            max_range=data["max_range"],
+        )
+    
+    def to_json(self):
+        return {
+            "id": self._id,
+            "name": self._name,
+            "capacity": self._capacity,
+            "max_range": self._max_range,
+            "status": self._status,
+            "assigned_route": self._assigned_route
+        }
 
     @property
     def id(self):
@@ -30,6 +50,14 @@ class Truck:
     @property
     def status(self):
         return self._status
+    
+    @property
+    def location(self):
+        return self._location
+    
+    @location.setter
+    def location(self, new_location):
+        self._location = new_location
 
     def is_free(self) -> bool:
         if self._assigned_route == None:
@@ -38,6 +66,10 @@ class Truck:
     def assign_to_route(self, route):
         self._assigned_route = route
         self._status = "Busy"
+    
+    def remove_from_route(self):
+        self._assigned_route = None
+        self._status = "Free"
 
     def __str__(self):
         return f'Truck with ID: {self._id}, Name: {self.name}, Capacity: {self.capacity}, Range: {self.max_range} created'
