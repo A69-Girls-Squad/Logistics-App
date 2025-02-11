ed3b2d9fa5e4fda547518bffa568f605d9b5759d
dilyanab <bozhinova.dilyana@gmail.com>
2025-02-11 13:42:21 +0200

    updating route and remove_truck

---
+++ commands/remove_truck_from_route.py
@@ -0,0 +1,25 @@
+from core.application_data import ApplicationData
+from commands.validation_helpers import validate_params_count
+from base_command import BaseCommand
+
+class RemoveTruckFromRouteCommand(BaseCommand):
+    def __init__(self, params, app_data: ApplicationData):
+        super().__init__(params, app_data)
+        self.params = params
+        self._app_data = app_data
+        
+
+    def execute(self):
+        validate_params_count(self.params, 2)
+        truck_id, route_id = self.params
+
+        truck = self.app_data.find_truck_by_id(truck_id)
+        route = self._app_data.find_route_by_id(route_id)
+
+        if truck.assigned_route != route:
+                raise ValueError(f'The selected truck is not assigned to the specified route')
+        
+        truck.remove_from_route()
+
+        return f"Truck with ID {truck_id} has been removed from Route {route_id}, truck status changed to free."
+
