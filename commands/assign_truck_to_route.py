8e9b9e28c414534e7fcfa58fc0b2e29961bcfa64
dilyanab <bozhinova.dilyana@gmail.com>
2025-02-12 09:16:20 +0200

    addin logger to assign_truck

--- commands/assign_truck_to_route.py
+++ commands/assign_truck_to_route.py
@@ -1,17 +1,20 @@
+import logging
 from core.application_data import ApplicationData
 from commands.validation_helpers import validate_params_count
 from base_command import BaseCommand
 
+logger = logging.getLogger(__name__)
 class AssignTruckToRouteCommand(BaseCommand):
     def __init__(self, params, app_data: ApplicationData):
         super().__init__(params, app_data)
         self.params = params
         self._app_data = app_data
+        
 
     def execute(self):
         validate_params_count(self.params, 2)
         truck_id, route_id = self.params
-
+        
         # get truck and route from classes or app data?
         truck = self.app_data.find_truck_by_id(truck_id)
         route = self._app_data.find_route_by_id(route_id)
@@ -27,6 +30,8 @@ class AssignTruckToRouteCommand(BaseCommand):
         
         route.assign_truck(truck)
         truck.assign_to_route(route)
-
+        
+        logger.info(f"Truck with id {truck_id} assigned to route {route_id}")
         return f"Truck with id {truck_id} assigned to route {route_id}"
+        
 
