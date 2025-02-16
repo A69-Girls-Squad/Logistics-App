0292d6cba9abe53fa880719ae2cd61e161056642
Irina.Parvanova <irina.p.parvanova@gmail.com>
2025-02-16 19:01:19 +0200

    fixed show packages

--- skeleton/models/constants/assign_status.py
+++ skeleton/models/constants/assign_status.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 
 class AssignStatus(Enum):
-    ASSIGNED = 'Assigned'
-    UNASSIGNED = 'Unassigned'
-    ALL = 'All'
+    ASSIGNED = 'assigned'
+    UNASSIGNED = 'unassigned'
+    ALL = 'all'
