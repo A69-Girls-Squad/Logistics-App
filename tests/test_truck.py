1368ec495ae809a6ceeedb6304d362c642cf474d
dilyanab <bozhinova.dilyana@gmail.com>
2025-02-17 10:03:33 +0200

    fix

--- tests/test_truck.py
+++ tests/test_truck.py
@@ -1,13 +1,14 @@
 import unittest
 from unittest.mock import MagicMock
 from skeleton.models.truck import Truck
+from skeleton.errors.application_error import ApplicationError
 
 class Truck_Should(unittest.TestCase):
 
     def test_init(self):
         self.truck_data = {
             "id": 1,
-            "name": "Truck A",
+            "name": "Truck Scania",
             "capacity": 2000,
             "max_range": 5000
         }
@@ -22,7 +23,7 @@ class Truck_Should(unittest.TestCase):
         self.assertEqual(self.truck.status, Truck.STATUS_FREE)
         self.assertIsNone(self.truck.assigned_route)
 
-    def test_to_json(self):
+    def test_to_json(self): # TO DO 
         pass
 
     def test_is_free(self):
@@ -39,7 +40,38 @@ class Truck_Should(unittest.TestCase):
         self.assertFalse(self.truck.is_free(route))
 
     def test_assign_to_route(self):
-        pass
+
+        route = MagicMock()
+        route.dparture_time = 9
+        route.Expected_arrival_time = 15
+        route.load = 500
+        route.distance = 1000
+
+        self.truck.assign_to_route(route)
+
+        self.assertEqual(self.truck.status, Truck.STATUS_BUSY)
+        self.assertEqual(self.truck.assign_to_route, route)
 
     def test_remove_from_route(self):
-        pass
+        route = MagicMock()
+        route.dparture_time = 9
+        route.Expected_arrival_time = 15
+        route.load = 500
+        route.distance = 1000
+
+        self.truck.assign_to_route(route)
+
+        self.assertEqual(self.truck.status, Truck.STATUS_FREE)
+        self.assertEqual(self.truck.assign_to_route, route)
+
+    def test_invalid_name(self):
+        with self.assertRaises(ApplicationError):
+            Truck(1001, "", 5000, 6000)
+
+    def test_invalid_capacity(self):
+        with self.assertRaises(ApplicationError):
+            Truck(1001, "Scania", -5000, 6000)
+
+    def test_invalid_max_range(self):
+        with self.assertRaises(ApplicationError):
+            Truck(1001, "", 5000, -6000)
